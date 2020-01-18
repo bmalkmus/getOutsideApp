5d534312c66cd09de1829451726c13f06c013037
Matt Benson <rabenson6@msn.com>
2020-01-18 13:01:11 -0800

    working feature search

--- script/api.js
+++ script/api.js
@@ -1,6 +1,35 @@
+
 $(document).ready(function(){
-    $('.button-search').click(function() {
+    var queryURL = "https://data.seattle.gov/resource/j9km-ydkc.json?";
+    var results = $('.container');
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response){
+        
+        var i;
+        var featuresList = [];
+        for (i = 0; i < response.length; i++) {
+            featuresList.push(response[i]['feature_desc']);
+            }
+            featuresList.sort();
+            var newfeaturesList = featuresList.filter(function(elem, index, self) {
+                return index === self.indexOf(elem);
+            });
+            newfeaturesList.forEach(element => $("#features-input").append("<option value=\""+element+"\">"+element+"</option>"));
+            
+
+    
+    
+})});
+
+
+
+
+$(document).ready(function(){
+    $('.park-search').click(function() {
         var inputText = $('#search-input').val();
+        console.log(inputText);
         var queryURL = "https://data.seattle.gov/resource/j9km-ydkc.json?name=" + inputText;
         var results = $('.container');
         
@@ -10,7 +39,18 @@ $(document).ready(function(){
         }).then(function(response){
             var responseString = JSON.stringify(response);
             results.text(responseString);
-            console.log(response);
         });
     });
+    $('.features-search').click(function() {
+        var featureText = $('#features-input').val();
+        console.log(featureText);
+        var queryURL = "https://data.seattle.gov/resource/j9km-ydkc.json?feature_desc=" + featureText;
+        var results = $('.container');
+        $.ajax({
+            url:queryURL,
+            method:"GET"
+        }).then(function(response){
+            var responseString = JSON.stringify(response);
+            results.text(responseString);
   });
+})});

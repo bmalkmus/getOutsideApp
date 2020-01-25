9f01db8983c849959adbaf49f42d15562657b8e3
nunetang <38163311+nunetang@users.noreply.github.com>
2020-01-24 21:22:04 -0800

    Nunetang/zomato (#9)
    * Adding a function for park results card
    
    * Added zomato API call
    
    * Jan22 master (#10)
    
    * Zomato API key
    
    * created park information to be displayed on loading screen based off the park closest to your location
    
    Co-authored-by: bmalkmus <57015175+bmalkmus@users.noreply.github.com>

--- script/api.js
+++ script/api.js
@@ -1,15 +1,127 @@
+function createParkResults(parkName){
+    let parkDiv = $('<div>');
+    $(parkDiv).addClass("park");
+    $(parkDiv).addClass("card blue-grey darken-1");
+    let parkCard = $('<div>');
+    $(parkCard).addClass("card-content white-text");
+    $(parkDiv).append(parkCard);
+    let parkSpan = $('<span>').text(parkName);
+    $(parkSpan).addClass("card-title");
+    $(parkCard).append(parkSpan);
+    let parkImgDiv = $('<div>');
+    $(parkDiv).append(parkImgDiv);
+    let parkImg = $('<img>');
+    $(parkImg).addClass("imgSmall");
+    $(parkImg).attr('id', 'imgSmall');
+    $(parkImg).attr('width', '200');
+    $(parkImg).attr('height', '100');
+    $(parkImg).attr('alt', 'Park Picture');
+    $(parkImgDiv).append(parkImg);
+    
+    return parkDiv;
+}
+
+
 $(document).ready(function(){
-    if (navigator.geolocation) {
+
+let ClosestList
+var queryURL = "https://data.seattle.gov/resource/j9km-ydkc.json?"
+
+if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(successFunction, errorFunction);
     }
-    else{
-        alert("Geolocation is not allowed")}
 
 
+        
+        
 function successFunction(position) {
     let currentLat = position.coords.latitude;
     let currentLon = position.coords.longitude;
+ 
+    // Upon loading, this is the code to populate some of the information for the closest park from the user computer
+    $.ajax({
+        url:queryURL,
+        method:"GET"
+    })
+            
+    .then(function(response){
+        ClosestList = [];
+    
+        for (i = 0; i < response.length; i++){
+            parkLat = response[i].ypos;
+            parkLon = response[i].xpos;
+            var radlat1 = Math.PI * currentLat/180;
+            var radlat2 = Math.PI * parkLat/180;
+            var theta = currentLon-parkLon;
+            var radtheta = Math.PI * theta/180;
+            var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+            if (dist > 1) {
+                    dist = 1;
+            }
+                dist = Math.acos(dist);
+                dist = dist * 180/Math.PI;
+                dist = dist * 60 * 1.1515;
+        
+                let parkName =response[i].name;
+                let parkResult = {parkName, dist};
+                ClosestList.push(parkResult);
+        }
+                 
+                
+        filteredList = $.grep(ClosestList, function(v) {
+            return v.dist > .0001;
 
+        });
+        
+        filteredList.sort(function(a,b){
+            return a.dist - b.dist
+        });
+
+        let longDist = filteredList[0].dist
+        let roundedDist = longDist.toFixed(2);
+
+        let inputText = filteredList[0].parkName;
+        let queryURL = "https://data.seattle.gov/resource/j9km-ydkc.json?name=" + inputText;
+        let results = $('#results-container');
+    
+        $.ajax({
+            url:queryURL,
+            method:"GET"
+        }).then(function(response){
+            console.log(response[0].feature_desc);
+             let closetPark = $('<h3>').text('Closest Park to your Current Location: '+inputText);
+             let parkFeat = $('<div>')
+
+            let featTitle = $('<h4>').text(("Park Features:"))
+             let parkHours = $('<h4>').text('Hours: ' + response[0].hours);
+             let parkDist = $('<h4>').text ('Distance to Park: ' + roundedDist + ' miles');
+            $(parkFeat).append(featTitle);
+             for (i=0; i < response.length; i ++){
+                 let singleFeat = $('<p>').text(response[i].feature_desc);
+                 console.log (singleFeat);
+                 $(parkFeat).append(singleFeat);
+             }
+            $(results).append(closetPark);
+            $(results).append(parkFeat);
+            //  console.log(parkFeat);
+            $(results).append(parkHours);
+            $(results).append(parkDist);
+
+
+
+            // var responseString = JSON.stringify(response);
+            // results.text(responseString);
+        });
+    });
+
+
+    else{
+        alert("Geolocation is not allowed")}
+
+    var currentLat = 0;
+    var currentLon = 0;
+    
+ 
     $('#search').click(function() {
         var inputText = $('.validate').val();
         var queryURL = "https://data.seattle.gov/resource/j9km-ydkc.json?name=" + inputText;
@@ -23,7 +135,6 @@ function successFunction(position) {
         .then(function(response){
             var responseString = JSON.stringify(response);
             results.text(responseString);
-            // console.log(response);
         });
     });
 
@@ -48,41 +159,35 @@ function successFunction(position) {
             newfeaturesList.forEach(element => $("#parkfeatures").append("<option value=\""+element+"\">"+element+"</option>"));   
 })
 
-// $('.park-search').click(function() {
-//     var inputText = $('#search-input').val();
-//     console.log(inputText);
-//     var queryURL = "https://data.seattle.gov/resource/j9km-ydkc.json?name=" + inputText;
-//     var results = $('#results');
-    
-//     $.ajax({
-//         url:queryURL,
-//         method:"GET"
-//     }).then(function(response){
-//         var responseString = JSON.stringify(response);
-//         results.text(responseString);
-//     });
-// });
+
+
 $('#parkfeatures').click(function() {
     var featureText = $('#parkfeatures').val();
-    console.log(featureText);
+    $('#results-container').empty();
     var queryURL = "https://data.seattle.gov/resource/j9km-ydkc.json?feature_desc=" + featureText;
-    var results = $('#results');
+    var results = $('#results-container');
     $.ajax({
         url:queryURL,
         method:"GET"
     }).then(function(response){
-        var responseString = JSON.stringify(response);
-        results.text(responseString);
+        // var responseString = JSON.stringify(response);
+        // results.text(responseString);
+        for (i = 0; i < response.length; i ++){
+            results.append(response[i].name+"<br>")
+        }
 });
 })
 
+function successFunction(position) {
+    currentLat = position.coords.latitude;
+    currentLon = position.coords.longitude;
+
+    console.log(position);
+
 $("#maxDistance").change(function(){
-    $('#results').empty();
-    console.log(this);
+    $('#results-container').empty();
     var inputText =$('#maxDistance').val();
     var queryURL = "https://data.seattle.gov/resource/j9km-ydkc.json?"
-    // var results = $('.container');
-
     
     $.ajax({
         url:queryURL,
@@ -91,6 +196,7 @@ $("#maxDistance").change(function(){
     
     .then(function(response){
         let DistanceList =[];
+        ClosestList = [];
         for (i = 0; i < response.length; i++){
             parkLat = response[i].ypos;
             parkLon = response[i].xpos;
@@ -105,26 +211,44 @@ $("#maxDistance").change(function(){
 		dist = Math.acos(dist);
 		dist = dist * 180/Math.PI;
         dist = dist * 60 * 1.1515;
-    
-        
+
+        let parkName =response[i].name;
+        let parkResult = {parkName, dist};
+        ClosestList.push(parkResult);
+
+
         if (!dist == NaN || dist <= inputText){
 
             DistanceList.push(response[i].name);
         }
         }
+         filteredList = $.grep(ClosestList, function(v) {
+            return v.dist > .0001;
+
+        });
+
+        filteredList.sort(function(a,b){
+            return a.dist - b.dist
+        });
+
+        console.log(filteredList)
+
+
+
+        
+
+
         if(DistanceList.length == 0){
             DistanceList.push("No parks within your mile search of current location")
         }
         Unique = [...new Set(DistanceList)];
 
         Unique.sort();
-
-        for (i = 0; i < Unique.length; i++){
+        for (i = 0; i < 5; i++){
             console.log(Unique[i]);
-            let parkDiv = $('<div>').text(Unique[i])
-            $(parkDiv).addClass("park");
+            let parkDiv = createParkResults(Unique[i]);
 
-            $('.container').append(parkDiv );
+            $('.results-container').append(parkDiv );
         }
        
 
@@ -135,24 +259,40 @@ $("#maxDistance").change(function(){
 
 }
 
-function errorFunction(error){
-    switch(error.code) {
-        case error.PERMISSION_DENIED:
-          alert("User denied the request for Geolocation.")
-          break;
-        case error.POSITION_UNAVAILABLE:
-          alert("Location information is unavailable.")
-          break;
-        case error.TIMEOUT:
-          alert("The request to get user location timed out.")
-          break;
-        case error.UNKNOWN_ERROR:
-          alert("An unknown error occurred.")
-          break;
-      }
-}
+    function errorFunction(error){
+        switch(error.code) {
+            case error.PERMISSION_DENIED:
+            alert("User denied the request for Geolocation.")
+            break;
+            case error.POSITION_UNAVAILABLE:
+            alert("Location information is unavailable.")
+            break;
+            case error.TIMEOUT:
+            alert("The request to get user location timed out.")
+            break;
+            case error.UNKNOWN_ERROR:
+            alert("An unknown error occurred.")
+            break;
+        }
+    }
+
+    // event handler for Rest. search
+
+    $(document).on("click", ".park", function(){
+        const c = 10;
+        var zomatoApiKey = 'f56d7ccb219fb8cce1bdc7e70b526b2f';
+        var queryURL = "https://developers.zomato.com/api/v2.1/search?entity_type=city&count=" + c + "&";
+        queryURL += 'lat=' + currentLat + '&lon=' + currentLon;
 
 
-
-
-})
+        $.ajax({
+            method: "GET",
+            url:queryURL,
+            headers: { "user-key": zomatoApiKey },
+        })
+        .then(function(response){
+            console.log(response);
+            $("#restaurants").text(JSON.stringify(response));
+        });
+    })
+});
